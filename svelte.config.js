18c4b3e1f82618399b0682aa5dd659a7dadfd305
Chitsanupong Rongpan <pp.rongpan@gmail.com>
2022-04-12 07:09:07 +0000

    Revert "Fix endless reloading"
    This reverts commit cfd6d481dc148284cd0a3cdeff88c68276442865.

--- svelte.config.js
+++ svelte.config.js
@@ -3,17 +3,8 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-        adapter: adapter(),
-        vite: {
-            server: {
-                hmr: {
-                    protocol: 'ws',
-                }
-            }
-        },
-        // hydrate the <div id="svelte"> element in src/app.html
-        target: '#svelte'
-    }
+		adapter: adapter()
+	}
 };
 
 export default config;
