e04d19d35baa6a2ed7e81cf81dc1d06ac4e413f5
Chitsanupong Rongpan <pp.rongpan@gmail.com>
2022-04-12 05:03:45 +0000

    Add a bit of timer thing.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,36 @@
 <script>
-    var seconds = 0
-    let display = "0.000"
+    var ms = 0
+    var i = 0
+    let display = "0.00"
+    
     function Seconds() {
-        seconds += 0.001;
-        display = seconds.toFixed(3)
+        if (i == 1) {
+            ms += 1
+            display = ms.toString().substring(0, ms.toString().length - 2) + "." + ms.toString().substring(ms.toString().length - 2, ms.toString().length);
+            if (display[0] == "."){
+                display = "0".concat(display)
+            }
+        }
     }
-    var cancel = setInterval(Seconds, 1);
+    function toggle(){
+        if (i == 0){
+            i = 1
+        } else {
+            i = 0
+        }
+    }
+    function reset(){
+        ms = 0
+        display = "0.00"
+    }
+    setInterval(Seconds, 1)
+
 </script>
 
 <h1 class="text-blue-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a class="hover:underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>You are here <b class="text-blue-500">{display}</b> s</p>
+<div class="flex space-x-4">
+    <button on:click={toggle} class="transition-colors duration-300 text-gray-500 {i == 0 ? 'bg-gray-100 hover:bg-gray-200' : 'bg-gray-300'} rounded px-3 py-2">{#if i == 0}Start{:else}Stop{/if}</button>
+    <button on:click={reset} class="text-white bg-orange-400 rounded px-3 py-2">Reset</button>
+</div>
